8ad84456ecde8ce72809b4fe1f5a72908a19aeea
Tony <tonyrkovar@gmail.com>
2019-10-31 15:21:13 -0500

    added env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server');
 
-server.listen(6000, () => {
+const port = process.env.PORT || 6000;
+
+server.listen(port, () => {
     console.log('Server is on')
 })
